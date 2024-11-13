c41488e6d3a354f84f2c62867893b95d4be286e3
arjunseven <mallikotipalli7@gmail.com>
2024-11-13 16:12:43 +0530

    Initial commit with GitHub Pages setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Melcowe to Arjun's React Website</h1>
+      <p>This is a simple React app deployed on GitHub Pages.</p>
+    </div>
+  );
+}
+
+export default App;
